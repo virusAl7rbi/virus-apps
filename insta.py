5cc17f7f6b02e47e03f67be1b621b9767d3d4d21
virus al7rbi <35959050+virusAl7rbi@users.noreply.github.com>
2020-10-17 10:07:10 +0300

    Update insta.py

--- insta.py
+++ insta.py
@@ -1,5 +1,4 @@
-from flask import request
-from flask import Flask
+from flask import request, jsonify, Flask
 import sqlite3
 import socket
 
@@ -7,6 +6,13 @@ conn = sqlite3.connect('insta.db', check_same_thread=False)
 cursor = conn.cursor()
 
 app = Flask(__name__)
+
+def ipv4():
+	s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+	s.connect(("8.8.8.8", 80))
+	ipv4 = s.getsockname()[0]
+	s.close()
+	return ipv4
 
 
 @app.route('/Apps', methods=['GET'])
@@ -22,8 +28,19 @@ def start():
 		return f"<script>window.open('{url}','_self');</script>"
 
 
+@app.route('/Add', methods=["POST"])
+def AddToDB():
+	appid = request.args.get('id')
+	name = request.args.get('name')
+	androurl = request.args.get('androurl')
+	iosurl = request.args.get('iosurl')
+	data = """
+	INSERT INTO Insta VALUES ('{code}','{name}','{android}','{ios}');
+	""".format(code=appid, name=name, android=androurl, ios=iosurl)
+	cursor.execute(data)
+	conn.commit()
+	return jsonify(success=True)
+
+
 if __name__ == '__main__':
-	s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-	s.connect(("8.8.8.8", 80))
-	local_ip = str(s.getsockname()[0])
-	app.run(host=local_ip, port=80)
+	app.run(port=80)
